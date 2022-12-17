1810892eb418cffd19c1e5f16146607b7b5ecb79
Artem Prokofiev <artem-prokofiev@yandex.ru>
2022-12-18 01:28:13 +0400

    extended movie model

--- Cataloger.Models/Movie.cs
+++ Cataloger.Models/Movie.cs
@@ -4,4 +4,8 @@ public class Movie
 {
     public int Id { get; set; }
     public string? Title { get; set; }
+     public string? NameDirector { get; set; }
+    public string? Year { get; set; }
+    public string? Country { get; set; }
+
 }
